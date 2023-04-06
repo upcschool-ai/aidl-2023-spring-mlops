f26f282cd0a4a99e4b6b252d3af9d898162805f1
Juan José Nieto <juanjo.nieto@glovoapp.com>
2023-04-06 18:02:27 +0200

    feat: add session-5

---
+++ session-5/Logger.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+from typing import Optional
+import matplotlib.pyplot as plt
+
+class Logger:
+
+    def log_reconstruction_training(
+        self,
+        model: nn.Module,
+        epoch: int,
+        train_loss_avg: float,
+        val_loss_avg: float,
+        reconstruction_grid: Optional[torch.Tensor] = None,
+    ):
+        raise NotImplementedError
+
+    def log_classification_training(
+        self,
+        model: nn.Module,
+        epoch: int,
+        train_loss_avg: float,
+        val_loss_avg: float,
+        train_acc_avg: float,
+        val_acc_avg: float,
+        fig: plt.Figure,
+    ):
+        raise NotImplementedError
+
+    def log_model_graph(
+        self,
+        model: nn.Module,
+        train_loader: torch.utils.data.DataLoader,
+    ):
+        raise NotImplementedError
+
+    def log_embeddings(
+        self,
+        model: nn.Module,
+        train_loader: torch.utils.data.DataLoader,
+    ):
+        raise NotImplementedError
